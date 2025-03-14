cbbaba299a7c780eda407089dee344fc62f4d212
leontoys <svliyon@gmail.com>
2025-03-14 14:20:21 +1100

    added-error-notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react'
 import ItemList from './components/ItemList'
 import './App.css'
+import Notification from './components/Notification'
 
 function App() {
 
   //Constants
   const defaultUrl = 'https://feeds.buzzsprout.com/2289931.rss'
   const proxyUrl = "https://api.allorigins.win/get" //'https://cors-anywhere.herokuapp.com/' 
-  const defaultAudioUrl = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/jfk.wav' 
 
   //State Variables
   const [url,setUrl] = useState("")
   const [items,setItems] = useState([]) 
+  const [message,setMessage] = useState("")
 
   const fetchRSSFeed = async (url)=>{
     try {
@@ -24,14 +25,17 @@ function App() {
     //return contents
     return json.contents    
     } catch (error) {
-      console.error(error)
+      console.error("error",error.message)
+      setMessage(error.message)
+      setTimeout(()=>setMessage(""),2000)
     }
   }
 
   const handleClick = async (e)=>{
+    try {
     //console.log(url||defaultUrl)
     //fetch the feed 
-    const contents = await fetchRSSFeed(url||defaultUrl)
+    const contents = await fetchRSSFeed(url)
     console.log("contents",contents)
     //now we need to parse the feed
     const xml = new window.DOMParser().parseFromString(contents,'application/xml')
@@ -41,17 +45,20 @@ function App() {
     const items_array = Array.from(items_html)
     //console.log("items array",items_array)
     setItems(items_array)//add items
-    //TODO-clean up this - loop through the feeds
-    //for(let i = 0; i < items_array.length; i++){
-    //  console.log(items_array[i].getElementsByTagName("title")[0].textContent)
-    //}
+      
+    } catch (error) {
+      console.error("error",error)
+      setMessage(error.message)
+      setTimeout(()=>setMessage(""),2000)
+    }
   }
 
   return (
       <div className="card">
         <h1>RSS Feed Reader</h1>
-        <input type='text' value={url||defaultUrl} onChange={e=>setUrl(e.target.value)}></input>
+        <input type='text' value={url} onChange={e=>setUrl(e.target.value)}></input>
         <button type='submit' onClick={handleClick}>Get Feeds</button>
+        <Notification message={message}></Notification>
         <ItemList items={items}/>
       </div>
   )
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,12 +3,12 @@ const ItemList = ({ items , onClick }) => {
         <div className="item-list">
         {
             items.map(item => (
-                <a href="#" key={item.getElementsByTagName("title")[0].textContent}>
+                <a href={item.getElementsByTagName("link")[0].textContent} key={item.getElementsByTagName("guid")[0].textContent}>
                 <div className="item">
                     <h2>{item.getElementsByTagName("title")[0].textContent}</h2>
-                    <p>{item.getElementsByTagName("itunes:summary")[0].textContent}</p>
+ {/*                    <p>{item.getElementsByTagName("itunes:summary")[0].textContent}</p>
                     <audio controls src={item.getElementsByTagName("enclosure")[0].getAttribute("url")}></audio>
-                    </div>
+  */}                   </div>
                 </a>
             ))
         }
